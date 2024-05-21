b4a0b8708c059fa7be85f6fcbdbcf0aae29fec74
Lee Briggs <lee@leebriggs.co.uk>
2024-05-21 08:06:55 -0700

    move main resources in main.tf

--- main.tf
+++ main.tf
@@ -21,3 +21,20 @@ resource "aws_ecs_service" "tailscale" {
     subnets          = var.subnet_ids
   }
 }
+
+resource "aws_ecs_task_definition" "tailscale" {
+  family                   = var.name
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = var.cpu
+  memory                   = var.memory
+  execution_role_arn       = aws_iam_role.exec_role.arn
+
+  container_definitions = local.container_definition
+
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = var.cpu_architecture
+  }
+}
